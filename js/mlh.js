0a3135cd029f0f2ab31094672122f354129bde33
Joseph Musser <joemussa@gmail.com>
2018-01-16 01:13:40 -0600

    Website is fixed.  jQuery.slim doesnt work in my use case.

--- js/mlh.js
+++ js/mlh.js
@@ -6,4 +6,7 @@ var toMLH = function() {
 
     // URL for 'Submit Project!' button
     // http://devpost.hackisu.org
+
+
+    //http://my.mlh.io/oauth/authorize?client_id=3e2c46cf48f942d94c7aa1c04c5c0186fbfb2dbb1822a21928a7bcf1372bc51e&redirect_uri=https%3A%2F%2Fhackisu.org%2Fcallback.html&response_type=token
   }
