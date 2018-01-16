22b13bd62eae054c04d4b0934da7c956fa3e3cf3
Joseph Musser <joemussa@gmail.com>
2018-01-16 00:36:49 -0600

    New website finished!

---
+++ js/mlh.js
@@ -0,0 +1,9 @@
+var toMLH = function() {
+    window.location.assign("http://my.mlh.io/oauth/authorize?client_id=" + "3e2c46cf48f942d94c7aa1c04c5c0186fbfb2dbb1822a21928a7bcf1372bc51e" + "&redirect_uri=" + "https%3A%2F%2Fhackisu.org%2Fcallback.html" + "&response_type=token");
+
+    // URL for 'Register' button
+    // http://my.mlh.io/oauth/authorize?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URL+"&response_type=token
+
+    // URL for 'Submit Project!' button
+    // http://devpost.hackisu.org
+  }
