1a82a8a39a1317ec16cb1d1d7ba314e71c59bf02
Mitchell <mdpung@gmail.com>
2018-07-09 21:23:59 -0500

    fixed id

--- js/mlh.js
+++ js/mlh.js
@@ -1,5 +1,5 @@
 var toMLH = function() {
-    window.location.assign("http://my.mlh.io/oauth/authorize?client_id=" + "3e2c46cf48f942d94c7aa1c04c5c0186fbfb2dbb1822a21928a7bcf1372bc51e" + "&redirect_uri=" + "https%3A%2F%2Fhackisu.org%2Fcallback.html" + "&response_type=token");
+    window.location.assign("http://my.mlh.io/oauth/authorize?client_id=" + "4dc8323170a08d81856c8a11d2890abf660d0546021cfa2039e6bb26fe238558" + "&redirect_uri=" + "https%3A%2F%2Fhackisu.org%2Fcallback.html" + "&response_type=token");
 
     // URL for 'Register' button
     // http://my.mlh.io/oauth/authorize?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URL+"&response_type=token
